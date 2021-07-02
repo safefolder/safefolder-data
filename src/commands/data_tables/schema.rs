dd214630afcb44d6612f3f20fdb094a11ae51936
Jorge Alegre <me@jorgealegre.com>
2021-07-02 19:18:06 +0200

    Compile tests and start command create table

---
+++ src/commands/data_tables/schema.rs
@@ -0,0 +1,9 @@
+// This is the module with the functions that will create schema for the tables, with field definition that comes
+// From the work already done in December 2020.
+// 
+
+// Each db is having a table
+
+pub fn create_table() {
+    println!("create table command...")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+pub mod commands;
+
+use commands::data_tables::schema::create_table;
+
 fn main() {
     println!("Hello, world!");
+    create_table();
 }
